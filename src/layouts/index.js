ba4f838db65d6ac992f9f752284ff639d3028322
monkypuzzle <paul.newsam@gmail.com>
2017-09-23 21:00:36 -0700

    Removes non-generalizable content

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
-import "./main.scss";
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
+import './main.scss';
 
-import Navbar from "./Navbar";
-import Footer from "./Footer";
+import Navbar from './Navbar';
+import Footer from './Footer';
 
 const TemplateWrapper = ({ children }) => (
   <div>
@@ -12,21 +12,19 @@ const TemplateWrapper = ({ children }) => (
       title="Gatsby Default Starter"
       meta={[
         { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'keywords', content: 'sample, something' }
       ]}
     />
     <Navbar />
     <section className="section">
-      <div className="container">
-        {children()}
-      </div>
+      <div className="container">{children()}</div>
     </section>
     <Footer />
   </div>
-)
+);
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.func,
-}
+  children: PropTypes.func
+};
 
-export default TemplateWrapper
+export default TemplateWrapper;
