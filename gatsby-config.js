c6823ccdd8c56c5625b5aa6e05002cc7fd1d0a92
monkypuzzle <paul.newsam@gmail.com>
2017-09-23 20:58:16 -0700

    Adds styled-components with plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Gatsby Default Starter`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sass`
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-styled-components`
   ]
 };
