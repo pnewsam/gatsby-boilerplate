0544a70474825056f1d20da86d5e868f3ab7d93c
monkypuzzle <paul.newsam@gmail.com>
2017-09-23 23:27:33 -0700

    Adds SocialIcons component to footer

--- src/bulma/Footer.jsx
+++ src/bulma/Footer.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Link from 'gatsby-link';
+import SocialIcons from './SocialIcons';
 
 const Footer = props => (
   <footer className="footer">
     <div className="container">
-      <div className="content">{props.children}</div>
+      <div className="content has-text-centered">
+        <SocialIcons />
+        {props.children}
+      </div>
     </div>
   </footer>
 );
