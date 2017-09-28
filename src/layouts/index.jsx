45666c4d3c810f093313f508176adb108817f73a
monkypuzzle <paul.newsam@gmail.com>
2017-09-27 20:07:04 -0700

    Changes index.js to jsx

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
-import Link from 'gatsby-link';
 import './main.scss';
 
 import Navbar from '../components/Navbar/Navbar';
@@ -16,17 +15,7 @@ const TemplateWrapper = ({ children }) => (
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
-    <Navbar>
-      <Link to="" className="navbar-item">
-        Link
-      </Link>
-      <Link to="" className="navbar-item">
-        Link
-      </Link>
-      <Link to="" className="navbar-item">
-        Link
-      </Link>
-    </Navbar>
+    <Navbar />
     {children()}
     <Footer>&copy; Business Name 2017</Footer>
   </div>
