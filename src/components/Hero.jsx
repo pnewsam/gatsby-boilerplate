6528062ed7d983b1e90271a49dc9215e381223c5
monkypuzzle <paul.newsam@gmail.com>
2017-09-27 20:33:18 -0700

    Adds background-image css to Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
+// import hero from '../assets/hero.jpg';
+
+const StyledSection = styled.section`
+  background: url(${''}) no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+`;
 
 const Hero = props => (
-  <section className="hero is-primary is-large">
+  <StyledSection className="hero is-primary is-fullheight">
     <div className="hero-body">
       <div className="container">{props.children}</div>
     </div>
-  </section>
+  </StyledSection>
 );
 
 export default Hero;
