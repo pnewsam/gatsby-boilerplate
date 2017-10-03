2769bed4558b584f76f3307d026f16311ab9b5aa
monkypuzzle <paul.newsam@gmail.com>
2017-10-02 23:58:26 -0700

    Adds google-analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,16 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`
+    title: 'Gatsby Default Starter',
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sass`,
-    `gatsby-plugin-styled-components`
-  ]
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass',
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'YOUR_GOOGLE_ANALYTICS_TRACKING_ID',
+      },
+    },
+  ],
 };
